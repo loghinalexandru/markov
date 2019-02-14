c98102414abb07bd89c72569ef810d54c8b8160a
loghinalexandru <loghinalexandru61@gmail.com>
2019-02-14 17:44:16 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,30 @@
-let model =  new Markov(1);
+let model =  new Markov(2);
 
-function getNewsTitles(){
-	fetch("https://newsapi.org/v2/everything?pageSize=1&domains=bbc.co.uk&apiKey=37a072c5b2a94bbdac965b2f49a19ce8")
+function addNewsTitles(domains){
+	fetch("https://newsapi.org/v2/everything?pageSize=100&domains=" + domains + "&apiKey=37a072c5b2a94bbdac965b2f49a19ce8")
 	.then(response => {
 		return response.json()
 	})
 	.then(data => {
 		for(let i = 0; i < data.articles.length; ++i){
-			model.addToDictionary(data.articles[i].title);
+			model.addToDictionary(data.articles[i].content);
 		}
 	});
 
 }
 
 window.onload = () =>{
-	getNewsTitles()
+	addNewsTitles("bbc.co.uk");
+	addNewsTitles("wsj.com");
+	addNewsTitles("nytimes.com");
+	addNewsTitles("edition.cnn.com");
+	addNewsTitles("foxnews.com");
+	setInterval(insertGeneratedText , 10000);
 }
+
+function insertGeneratedText(){
+	let elem = document.getElementsByTagName("body")[0];
+	elem.innerHTML = model.generateText(1000);
+}
+
+
--- scripts/markov.js
+++ scripts/markov.js
@@ -1,9 +1,10 @@
 class Markov{
-	constructor(order = 1){
+	constructor(order = 1 , terminator = "_$_"){
 		this.order = order;
-		this.entryWords = new Set();
+		this.entryWords = [];
 		this.endWords = {};
 		this.dictionary = {};
+		this.terminator = terminator;
 	}
 
 	parsePhrase(phrase){
@@ -14,6 +15,8 @@ class Markov{
 				--i;
 			}
 		}
+		// Insert terminator for end of phrase
+		listOfWords.push(this.terminator);
 		return listOfWords;
 	}
 
@@ -30,7 +33,8 @@ class Markov{
 		if(words.length > this.order){
 			currentWordSequence = words.slice(0,this.order);
 			key = currentWordSequence.join(" ");
-			this.entryWords.add(key);
+			if(!this.entryWords.includes(key))
+				this.entryWords.push(key);
 		}
 		else{
 			throw "Too small phrase for order " + this.order;
@@ -47,26 +51,74 @@ class Markov{
 				this.dictionary[key][words[i]] = 1;
 
 			// Move entry one word further
-			currentWordSequence.push(words[i]);
-			currentWordSequence.splice(0 , 1);
+			this.cycleWordSequence(currentWordSequence , words[i]);
 
 		}
-		console.log(words , this.dictionary);
 	}
 
+	cycleWordSequence(list , word){
+		list.push(word);
+		list.splice(0 , 1);
+	}
+	// TODO : Rewrite code cleaner in generateText()
+	// TODO : Solve cycles in text generation
 	generateText(length){
 		if(length < this.order)
 			throw "Length too small for order " + this.order;
-		let currentWordSequence = randomItem(this.entryWords).split(" ");
-		let key = currentWordSequence.join(" ");
+		let currentWordSequence = randomItem(this.entryWords);
+		let constructedText = currentWordSequence;
+		let nextWord = "";
+		let key = "";
+		currentWordSequence = currentWordSequence.split(" ");
 		for(let i = length - this.order; i >= 0; --i){
-
+			key = currentWordSequence.join(" ");
+			let wordTransition = maxFromDictionary(this.dictionary[key]);
+			if(this.dictionary[key][wordTransition] === this.dictionary[minFromDictionary(this.dictionary[key])]) {
+				nextWord = randomItem(Object.keys(this.dictionary[key]));
+			}
+			else{
+				nextWord = wordTransition; 
+			}
+			if(nextWord !== this.terminator){
+				if(nextWord.length  > 1)
+					constructedText = constructedText + " " + nextWord;
+				else
+					constructedText = constructedText + nextWord;
+				this.cycleWordSequence(currentWordSequence , nextWord);
+			}
+			else{
+				break;
+			}
 		}
+		return constructedText;
 	}
-
 }
 
 
 function randomItem(list){
-	return list[Math.floor(Math.random * list.length)];
+	return list[Math.floor(Math.random() * list.length)];
+}
+
+function maxFromDictionary(dict){
+	let maxValueKey = -1;
+	let max = 0;
+	for(let key in dict){
+		if(dict[key] > max){
+			max = dict[key];
+			maxValueKey = key;
+		}
+	}
+	return maxValueKey;
+}
+
+function minFromDictionary(dict){
+	let minValueKey = -1;
+	let min = Number.MAX_SAFE_INTEGER;
+	for(let key in dict){
+		if(dict[key] < min){
+			min = dict[key];
+			minValueKey = key;
+		}
+	}
+	return minValueKey;
 }
